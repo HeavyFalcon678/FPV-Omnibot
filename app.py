5dfdac8292cc56e78ba985532680e6ab9f3ee461
HeavyFalcon678 <181516481+HeavyFalcon678@users.noreply.github.com>
2026-03-19 11:11:34 -0700

    Fix mobile issue

--- app.py
+++ app.py
@@ -4,17 +4,25 @@ from gpiozero import Device, OutputDevice
 from gpiozero.pins.mock import MockFactory
 
 
-Device.pin_factory = MockFactory()
+#Device.pin_factory = MockFactory()
 
-rf1 = OutputDevice(2)
-rf2 = OutputDevice(3)
-rf3 = OutputDevice(4)
-rf4 = OutputDevice(17)
-rf5 = OutputDevice(27)
-rf6 = OutputDevice(22)
-rf7 = OutputDevice(10)
-rf8 = OutputDevice(9)
+rf1 = OutputDevice(4)
+rf2 = OutputDevice(17)
+rf3 = OutputDevice(27)
+rf4 = OutputDevice(22)
+rf5 = OutputDevice(10)
+rf6 = OutputDevice(9)
+rf7 = OutputDevice(11)
+rf8 = OutputDevice(5)
 
+rf1.value = 1
+rf2.value = 1
+rf3.value = 1
+rf4.value = 1
+rf5.value = 1
+rf6.value = 1
+rf7.value = 1
+rf8.value = 1
 
 
 app = Flask(__name__)
@@ -88,16 +96,16 @@ def sendToArduino(signal):
     signals = signal.split(", ")
 
     print(signals)
-    rf1.value = signals[0]
-    rf2.value = signals[1]
-    rf3.value = signals[2]
-    rf4.value = signals[3]
-    rf5.value = signals[4]
-    rf6.value = signals[5]
-    rf7.value = signals[6]
-    rf8.value = signals[7]
+    rf1.value = int(signals[0])
+    rf2.value = int(signals[1])
+    rf3.value = int(signals[2])
+    rf4.value = int(signals[3])
+    rf5.value = int(signals[4])
+    rf6.value = int(signals[5])
+    rf7.value = int(signals[6])
+    rf8.value = int(signals[7])
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
