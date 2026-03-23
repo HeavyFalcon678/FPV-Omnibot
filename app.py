869f7c839bb65b96e886b1d5a90221a58327aa55
HeavyFalcon678 <181516481+HeavyFalcon678@users.noreply.github.com>
2026-03-23 11:00:59 -0700

    Revert to cv2

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, request, Response
 from gpiozero import Device, OutputDevice
 from picamera2 import Picamera2
-from picamera2.encoders import MJPEGEncoder
-from picamera2.outputs import FileOutput
-import io
-import threading
+import cv2
 
 
 
@@ -26,37 +23,20 @@ rf6.value = 1
 rf7.value = 1
 rf8.value = 1
 
-class StreamingOutput(io.BufferedIOBase):
-    def __init__(self):
-        self.frame = None
-        self.condition = threading.Condition()
-
-    def write(self, buf):
-        with self.condition:
-            self.frame = buf
-            self.condition.notify_all()
-
 
 app = Flask(__name__)
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration())
-
-output = StreamingOutput()
 picam2.start()
-
-picam2.start_recording(
-    MJPEGEncoder(),
-    FileOutput(output)
-)
 
 
 def generateFrames():
     while True:
-        with output.condition:
-                output.condition.wait()
-                frame = output.frame
-
+        frame = picam2.capture_array()
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        frame = cv2.flip(frame, -1)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
